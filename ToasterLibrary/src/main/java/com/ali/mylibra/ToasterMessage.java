a8bbe4d0d00ee2c67c72799204df547cc649b7d8
aliahmad39 <a.ahmad0315@gmail.com>
2021-12-09 16:58:32 +0500

    commit

---
+++ ToasterLibrary/src/main/java/com/ali/mylibra/ToasterMessage.java
@@ -0,0 +1,13 @@
+package com.ali.mylibra;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
